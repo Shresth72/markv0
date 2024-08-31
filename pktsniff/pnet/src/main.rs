cb91406c123f133eb84637961a311c154b398dfe
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-09-01 01:34:43 +0530

    ipv4

--- pktsniff/pnet/src/main.rs
+++ pktsniff/pnet/src/main.rs
@@ -1,5 +1,16 @@
 use pcap;
-use pnet;
+use pnet::{
+    self,
+    packet::{
+        ethernet::{EtherTypes, EthernetPacket},
+        ip::IpNextHeaderProtocols,
+        ipv4::Ipv4Packet,
+        // ipv6::Ipv6Packet,
+        tcp::TcpPacket,
+        udp::UdpPacket,
+        Packet,
+    },
+};
 
 fn main() {
     let interface = "eth0";
@@ -13,7 +24,43 @@ fn main() {
         .unwrap();
 
     while let Ok(packet) = cap.next_packet() {
-        println!("Received packet with length {}", packet.header.len);
-        // Add filtering and processing
+        // Parse the Ethernet frame from the captured packet data
+        if let Some(ethernet_packet) = EthernetPacket::new(&packet.data) {
+            match ethernet_packet.get_ethertype() {
+                EtherTypes::Ipv4 => {
+                    if let Some(ipv4_packet) = Ipv4Packet::new(ethernet_packet.payload()) {
+                        match ipv4_packet.get_next_level_protocol() {
+                            IpNextHeaderProtocols::Tcp => {
+                                if let Some(tcp_packet) = TcpPacket::new(ipv4_packet.payload()) {
+                                    println!(
+                                        "TCP Packet: {}:{} > {}:{}; Seq: {}, Ack: {}",
+                                        ipv4_packet.get_source(),
+                                        tcp_packet.get_source(),
+                                        ipv4_packet.get_destination(),
+                                        tcp_packet.get_destination(),
+                                        tcp_packet.get_sequence(),
+                                        tcp_packet.get_acknowledgement()
+                                    );
+                                }
+                            }
+                            IpNextHeaderProtocols::Udp => {
+                                if let Some(udp_packet) = UdpPacket::new(ipv4_packet.payload()) {
+                                    println!(
+                                        "UDP Packet: {}:{} > {}:{}; Len: {}",
+                                        ipv4_packet.get_source(),
+                                        udp_packet.get_source(),
+                                        ipv4_packet.get_destination(),
+                                        udp_packet.get_destination(),
+                                        udp_packet.get_length()
+                                    );
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
     }
 }
